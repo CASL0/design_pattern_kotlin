a7e16a3beb7235101bc2133e792ee1dced94670d
CASL0 <gin.0046.822@gmail.com>
2022-02-19 12:09:41 +0900

    initial commit

---
+++ app/src/main/java/jp/co/casl0/android/design_pattern_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.co.casl0.android.design_pattern_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
